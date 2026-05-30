6d0230e419370f5e378d4860614614894a354ef5
gkaravel <jmkaravel4@gmail.com>
2026-05-30 22:48:46 +0300

    chore: initialise project scaffold (package.json, vite.config.js, .gitignore) feat: add shared constants (HN_BASE, PAGE_SIZE, POLL_MS, MAX_CONCURRENT, FEED_TYPES)

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const HN_BASE    = 'https://hacker-news.firebaseio.com/v0';
+export const PAGE_SIZE  = 20;
+export const POLL_MS    = 5000;
+export const MAX_CONCURRENT = 6;
+export const FEED_TYPES = ['top', 'new', 'ask', 'show', 'job'];
