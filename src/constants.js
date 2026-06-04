58df57b8818cf0fa5bfaa23b073414d79aaad115
ckotsalas <christophorosk601@gmail.com>
2026-06-04 13:43:02 +0300

    feat(src/constants.js): add poll feed and HN search base

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const HN_BASE    = 'https://hacker-news.firebaseio.com/v0';
+export const HN_SEARCH_BASE = 'https://hn.algolia.com/api/v1';
 export const PAGE_SIZE  = 20;
 export const POLL_MS    = 5000;
 export const MAX_CONCURRENT = 6;
-export const FEED_TYPES = ['top', 'new', 'ask', 'show', 'job'];
+export const FEED_TYPES = ['top', 'new', 'ask', 'show', 'job', 'poll'];
